a216b27045978cb954fc3d24f6edf280c67c1b80
5456465tony97202 <109021079@live.asia.edu.tw>
2021-03-26 15:02:04 +0800

    0326

--- demo.py
+++ demo.py
@@ -1,8 +1,17 @@
 import requests as reqs
-
-r=reqs.get("http://210.70.80.21/~bs109021079/"
+from bs4 import BeautifulSoup
+req=reqs.get("http://210.70.80.21/~bs109021079/"
 )
-print(r.status_code)
+req.encoding="utf8"  #轉換為"utf8"
+if req.status_code == 200:
+   soup=BeautifulSoup(req.text,"lxml") #lxml呈現資料的格式
+   #print(soup)
+   result1=soup.find_all("li") 
+   print(result1)
+else:
+    print("no page")
 
 #終端機打python -m pip install requests
 #再安裝 python -m pip install BeautifulSoup4
+
+
